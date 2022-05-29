9ce5e98025372826f3285cf61e680f0efd742f8a
Joshua Carroll <joshcarroll21@gmail.com>
2022-05-29 11:41:05 -0500

    Refactor of gisreporter

---
+++ gisreporter_/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Text.Json;
+using System.Text;
+
+namespace gisreporter_
+{
+    class Program
+    {
+        private static string gisReceiverUrl = "https://giscommunicator.azurewebsites.net/api/gisreceiver?code=xNjuM3fBnIsZ28iTEiUNbFQcmBEdPkp5aspqxVm1aaJ6cGItH4dcyQ==";
+
+        private static List<MapItem> mapItems = new List<MapItem>();
+        private static Timer timer = new Timer();
+        private static bool continueRunning = true;
+        private static DateTime lastMessageReceived;
+
+        static void Main(string[] args)
+        {
+            lastMessageReceived = DateTime.Now.Subtract(new TimeSpan(30, 0, 0, 0));
+
+            Console.Write(@"
+/-------------------------------------------------------------------------\
+|                               GIS Reporter                              |
+|                       by Joshua Carroll, AA5JC                          |
+\_________________________________________________________________________/                                                                         
+
+This program will monitor the folders specified for Winlink mapping/GIS reports. If a new
+message is received that contains mapping data, it will send the details of the report
+to a web service for consolidation.
+
+(To view the data, visit https://aa5jc.com/map)
+
+Usage: dotnet gisreporter.dll [file path to your messages folder]
+  - Example: /W ''C:\RMS Express\AA5JC\Messages''
+
+Note: This can be added multiple times in case you have multiple callsigns, or want to monitor
+    a tactical address controlled from Paclink.
+  - Example: /W ''C:\RMS Express\AA5JC\Messages'' ''C:\Paclink\Accounts\DELTA_Account''
+
+");
+
+
+            CheckForNewData(args);
+
+            Task t = Task.Run(async () => {
+                do
+                {
+                    Console.Write(string.Format("\r{0}                                      ", DateTime.Now.ToUniversalTime()));
+
+                    int i = 60;
+                    do
+                    {
+                        await Task.Delay(1000);
+                        Console.Write(string.Format("\rChecking for new data in {0} seconds...  ", i--));
+                    }
+                    while (i > 0);
+                    Console.Write("\r                                                                 ");
+
+                    CheckForNewData(args);
+                } while (continueRunning);
+            });
+            t.Wait();
+        }
+
+        private static void CheckForNewData(string[] paths)
+        {
+            for (int i = 0; i < paths.Length; i++)
+            {
+                CheckWinlinkMessages(paths[i]);
+            }
+        }
+
+        // This is not currently used.
+        private static void CheckWinlinkKml(string winlinkPath)
+        {
+            if (winlinkPath != string.Empty)
+            {
+                // Validate file path
+                if (Directory.Exists(winlinkPath))
+                {
+                    if (File.Exists(String.Format("{0}/Winlink_Messages.kml", winlinkPath)))
+                    {
+
+                    }
+
+                    if (mapItems.Count > 0)
+                    {
+                        // Send to web service
+                        string json = JsonSerializer.Serialize(mapItems);
+                        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(gisReceiverUrl);
+                        req.Method = "POST";
+                        req.ContentType = "application/json";
+                        Stream stream = req.GetRequestStream();
+                        byte[] buffer = Encoding.UTF8.GetBytes(json);
+                        stream.Write(buffer, 0, buffer.Length);
+                        HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                        Console.WriteLine("Server response: " + res.StatusDescription);
+
+                        if (res.StatusCode == HttpStatusCode.OK)
+                        {
+                            mapItems.Clear();
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("** The Winlink folder you specified does not exist.");
+                    timer.Stop();
+                }
+            }
+        }
+
+        private static void CheckWinlinkMessages(string path)
+        {
+            if (path != string.Empty)
+            {
+                // Validate file path
+                if (Directory.Exists(path))
+                {
+                    // Get all files 
+                    string[] files = Directory.GetFiles(path, "*.mime");
+
+                    // Check each of these files to see if they are plotable and new
+                    DateTime newestNewMessageReceived = lastMessageReceived;
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        if (File.GetLastWriteTime(files[i]) > lastMessageReceived)
+                        {
+                            if (newestNewMessageReceived < File.GetLastWriteTime(files[i]))
+                            {
+                                newestNewMessageReceived = File.GetLastWriteTime(files[i]);
+                            }
+
+                            WinlinkMessage msg = new WinlinkMessage(File.ReadAllText(files[i]), Path.GetFileNameWithoutExtension(files[i]));
+
+                            if (msg.MessageXML != null && msg.MessageXML != string.Empty)
+                            {
+                                Console.WriteLine(String.Format("- {0} received from {1} at {2}", msg.MessageSubject, msg.SendersCallsign, msg.DateTimeSent));
+                                MapItem mapItem = msg.ToMapItem();
+                                mapItems.Add(mapItem);
+                            }
+
+                        }
+                    }
+
+                    lastMessageReceived = newestNewMessageReceived;
+
+                    if (mapItems.Count > 0)
+                    {
+                        // Send to web service
+
+                        string json = JsonSerializer.Serialize(mapItems);
+                        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(gisReceiverUrl);
+                        req.Method = "POST";
+                        req.ContentType = "application/json";
+                        Stream stream = req.GetRequestStream();
+                        byte[] buffer = Encoding.UTF8.GetBytes(json);
+                        stream.Write(buffer, 0, buffer.Length);
+                        HttpWebResponse res = new HttpWebResponse();
+
+                        try
+                        {
+                            res = (HttpWebResponse)req.GetResponse();
+                            Console.WriteLine("Server response: " + res.StatusDescription);
+
+                            if (res.StatusCode == HttpStatusCode.OK)
+                            {
+                                mapItems.Clear();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("SERVER ERROR: " + ex.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("** The Winlink folder you specified does not exist.");
+                    timer.Stop();
+                }
+            }
+        }
+    }
+}
+
---
+++ gisreporter_/SETUP.bat
@@ -0,0 +1,41 @@
+﻿
+echo off
+cls
+echo.
+echo.
+set /P callsign=What is the callsign that will receive messages? 
+echo echo off > RUN.BAT
+echo cls >> RUN.BAT
+echo dotnet gisreporter.dll "C:\RMS Express\%callsign%\Messages" >> RUN.BAT
+echo echo. >> RUN.BAT
+echo echo  ************************************************************ >> RUN.BAT
+echo echo  *                                                          * >> RUN.BAT
+echo echo  *   If the program did not run, you need to install the    * >> RUN.BAT
+echo echo  *     .Net Core 3.1 runtime.  You can download it at:      * >> RUN.BAT
+echo echo  *                                                          * >> RUN.BAT
+echo echo  *            https://aka.ms/dotnet-download                * >> RUN.BAT
+echo echo  *                                                          * >> RUN.BAT
+echo echo  ************************************************************ >> RUN.BAT
+echo pause >> RUN.BAT
+echo You can now run the program by executing RUN.BAT.
+echo.
+echo For instructions on how you can view the reports, visit https://aa5jc.com/map
+echo.
+pause
+REM echo ^<?xml version="1.0" encoding="UTF-8"?^> > REPORTS.KML
+REM echo ^<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom"^> >> REPORTS.KML
+REM echo ^<NetworkLink^> >> REPORTS.KML
+REM echo 	^<name^>%callsign% Winlink messages^</name^> >> REPORTS.KML
+REM echo 	^<description^>Digital messages and forms received over amateur radio^</description^> >> REPORTS.KML
+REM echo 	^<Link^> >> REPORTS.KML
+REM echo 		^<href^>https://giscommunicator.azurewebsites.net/api/gisreceiver?code=xNjuM3fBnIsZ28iTEiUNbFQcmBEdPkp5aspqxVm1aaJ6cGItH4dcyQ==^&amp;recipient=%callsign%^</href^> >> REPORTS.KML
+REM echo 		^<refreshMode^>onInterval^</refreshMode^> >> REPORTS.KML
+REM echo 		^<refreshInterval^>180^</refreshInterval^> >> REPORTS.KML
+REM echo 	^</Link^> >> REPORTS.KML
+REM echo ^</NetworkLink^> >> REPORTS.KML
+REM echo ^</kml^> >> REPORTS.KML
+REM echo.
+REM echo You can now run the program by executing RUN.BAT.
+REM echo.
+REM echo To view the reports, open REPORTS.KML in Google Earth. 
+REM echo.
