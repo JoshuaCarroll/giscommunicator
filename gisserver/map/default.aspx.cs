18fb2e0ea9e9454cb93c85ba8645bad7a1e1efea
Joshua Carroll <joshcarroll21@gmail.com>
2022-05-30 08:37:09 -0500

    Bug fix
    Fixes a problem that was caused by net names shorter than 15 characters

--- gisserver/map/default.aspx.cs
+++ gisserver/map/default.aspx.cs
@@ -128,11 +128,20 @@ namespace gisserver.map
 
                 string filename = "";
                 if (p.Winlink)
-                    filename += p.WinlinkRecipient.Replace(" ","").Replace(',','_') + "-";
+                    filename += p.WinlinkRecipient.Trim().Replace(" ","").Replace(',','_') + "-";
                 if (p.Weather)
-                    filename += p.WeatherState + "-";
+                    filename += p.WeatherState.Trim() + "-";
                 if (p.Netlogger)
-                    filename += p.NetloggerNetName.Substring(0, 15);
+                {
+                    if (p.NetloggerNetName.Length > 15)
+                    {
+                        filename += p.NetloggerNetName.Trim().Substring(0, 15);
+                    }
+                    else
+                    {
+                        filename += p.NetloggerNetName.Trim();
+                    }
+                }
 
                 string kml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
 <kml xmlns=""http://www.opengis.net/kml/2.2"" xmlns:gx=""http://www.google.com/kml/ext/2.2"" xmlns:kml=""http://www.opengis.net/kml/2.2"" xmlns:atom=""http://www.w3.org/2005/Atom"">
